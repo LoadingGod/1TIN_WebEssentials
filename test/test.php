5676b28793da135e4ed01e2e9d2999c9b4699b4f
Frankie Claessens <claessensfrankie@gmail.com>
2015-06-10 18:57:56 +0200

    Oef0607_CSS + iPhone

---
+++ test/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fraa
+ * Date: 10/06/15
+ * Time: 15:57
+ */
+
+$lijst = array('a' => 'aap', 'b' => 'banaan');
+
+foreach($lijst as $key=>$value)
+{
+    echo ($key . "\t" . $value . "\r\n");
+}
